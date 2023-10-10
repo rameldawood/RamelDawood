9431de92149f30791363aa60fc49f6ddd4aa5da8
rameldawood <147534927+rameldawood@users.noreply.github.com>
2023-10-10 18:40:16 -0500

    Add files via upload

--- indexing_process.py
+++ indexing_process.py
@@ -8,12 +8,15 @@ from index import Index
 
 
 def docs_from_json(json_file_location: str) -> DictDocumentStore:
-    doc_store = DictDocumentStore()
-    with open(json_file_location, "r") as json_file:
-        for line in json_file:
-            doc = Document.from_json(line)
-            doc_store.add_document(doc)
-    return doc_store
+   doc_store = DictDocumentStore()
+   with open(json_file_location, 'r') as json_file:
+       for line in json_file:
+           data = json.loads(line)
+           doc_id = data['doc_id']
+           text = data['text']
+           doc_store.add_document(Document(doc_id=doc_id, text=text))
+           
+           return doc_store
 
 
 
